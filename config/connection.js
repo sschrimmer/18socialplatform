4055a9df78986164149966bbafb79354101f9ea9
Steven <stevenschrimmer1@gmail.com>
2023-08-07 21:38:10 -0500

    readme

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/twittleDB', {
+mongoose.connect('mongodb://127.0.0.1:27017/18socialplatform', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
